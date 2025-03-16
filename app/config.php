21c2d77daa5c5902d14f13fb8dff24dc5a067366
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-03-16 13:36:33 -0600

    feat:variable global config
    se configuraron las variables globales y la conexion a la base de datos.

---
+++ app/config.php
@@ -0,0 +1,23 @@
+<?php
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'ienti');
+define('PASSWORD', 'ienti95');
+define('DB', 'ienti');
+define('APP_NAME', 'ienti & manwere');
+define('APP_URL', 'http://localhost/ienti-php/');
+define('KEY_API_MAPS', '');
+date_default_timezone_set(timezoneId: 'America/Mexico_City');
+
+$servidor = "mysql:host=" . SERVIDOR . ";dbname=" . DB;
+try {
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+} catch (PDOException $e) {
+    print_r($e);
+}
+
+
+$fechaHora = date(format: 'Y-m-d H:i:s');
+$fecha_actual = date(format: 'Y-m-d');
+$dia_actual = date(format: 'd');
+$mes_actual = date(format: 'm');
+$ano_actual = date(format: 'Y');
