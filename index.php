20f2bd75b2635cebabbd9ae771522ee4b31e7ca7
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-03-16 14:42:18 -0600

    feat:login and index views
    se crearon las vistas para login e index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'app/config.php';
+require_once 'layout/Main/Head.php';
+?>
+<!-- Content wrapper start -->
+<div class="content-wrapper">
+
+  <!-- Row start -->
+  <div class="row gx-3">
+    <div class="col-xl-12">
+      <div class="card">
+        <div class="card-header">
+          <div class="card-title">Contenido de Inicio</div>
+        </div>
+        <div class="card-body"></div>
+      </div>
+    </div>
+  </div>
+  <!-- Row end -->
+
+</div>
+<!-- Content wrapper end -->
+<?php
+require_once 'layout/Main/Footer.php';
+?>
---
+++ login/index.php
@@ -0,0 +1,69 @@
+<?php
+require_once 'app/config.php';
+?>
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Meta -->
+  <meta name="description" content="ienti & manwere dashboard" />
+  <link rel="shortcut icon" href="<?php echo APP_URL ?>/public/images/icon.ico" />
+
+  <!-- Title -->
+  <title><?php echo APP_NAME ?></title>
+
+  <!-- *************
+            ************ Common Css Files *************
+        ************ -->
+  <!-- Bootstrap css -->
+  <link rel="stylesheet" href="<?php echo APP_URL ?>/public/css/bootstrap.min.css" />
+
+  <!-- Bootstrap font icons css -->
+  <link rel="stylesheet" href="<?php echo APP_URL ?>/public/fonts/bootstrap/bootstrap-icons.css" />
+
+  <!-- Main css -->
+  <link rel="stylesheet" href="<?php echo APP_URL ?>/public/css/main.min.css" />
+
+  <!-- Login css -->
+  <link rel="stylesheet" href="<?php echo APP_URL ?>/public/css/login.css" />
+</head>
+
+<body class="login-container">
+  <!-- Login box start -->
+  <div class="container">
+    <form action="" id="login-form" autocomplete="off" method="post">
+      <div class="login-box rounded-2 p-5">
+        <div class="login-form">
+          <a href="index.html" class="login-logo mb-3">
+            <img src="<?php echo APP_URL ?>/public/images/logo.webp" alt="Logo ienti & manwere" />
+          </a>
+          <h5 class="fw-light mb-5">Inicia Sesión para acceder</h5>
+          <div class="mb-3">
+            <label class="form-label">Usuario</label>
+            <input id="usuario" name="usuario" type="text" class="form-control" placeholder="Ingresa tu usuario" />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Contraseña</label>
+            <input id="password" name="password" type="password" class="form-control" placeholder="Ingresa tu contraseña" />
+          </div>
+          <div class="d-flex align-items-center justify-content-between">
+            <a href="forgot-password.html" class="text-blue text-decoration-underline">Olvidaste tu contraseña?</a>
+          </div>
+          <div class="d-grid py-3">
+            <input type="hidden" name="enviar" value="si"></input>
+            <button type="submit" class="btn btn-lg btn-primary">
+              Iniciar Sesión
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+  <!-- Login box end -->
+</body>
+
+</html>
