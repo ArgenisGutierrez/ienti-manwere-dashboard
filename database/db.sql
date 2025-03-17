f17314e211322ebf06ccf7422396f1a25b9915e8
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-03-16 23:56:29 -0600

    feat:db add roles
    se agrego la tabla de roles

--- database/db.sql
+++ database/db.sql
@@ -16,3 +16,24 @@ INSERT INTO usuarios (
   'ientiadmin','Administrador','admin@ienti.com.mx','ientiadmin95*','2025-03-16 15:21:55','1'
 );
 
+CREATE TABLE roles (
+  id_rol INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_rol VARCHAR (255) NOT NULL,
+
+  fyh_creacion DATETIME NULL,
+  fyh_modificacion DATETIME NULL,
+  estado VARCHAR (11)
+)ENGINE=InnoDB;
+
+
+INSERT INTO roles (
+  nombre_rol,fyh_creacion,estado
+) VALUES ( 
+'ADMINISTRADOR','2025-03-16 23:52:02','1'
+);
+
+INSERT INTO roles (
+  nombre_rol,fyh_creacion,estado
+) VALUES ( 
+'USUARIO','2025-03-16 23:52:02','1'
+);
